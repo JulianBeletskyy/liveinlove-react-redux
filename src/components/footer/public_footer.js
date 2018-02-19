c5c4f071133c17e1f089001b6ad3ba2d73831b62
Kivt <vitaliy.baychura@gmail.com>
2018-02-19 10:59:08 +0200

    footer fix

--- src/components/footer/public_footer.js
+++ src/components/footer/public_footer.js
@@ -89,11 +89,11 @@ class PublicFooter extends Component {
                     </Row>*/}
 
                     <Row className="title">
-                        <Col sm={12} md={6}>
+                        <Col xs={6}>
                             Copyright &copy; Liveinlove - All Rights Reserved
                         </Col>
 
-                        <Col sm={12} md={6} className="text-right">
+                        <Col xs={6} className="text-right">
                             2018
                         </Col>
                     </Row>
