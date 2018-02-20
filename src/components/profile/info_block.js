d63fe58b0e26edfcd97cfc79db8c97ba37eb6ae8
Kivt <vitaliy.baychura@gmail.com>
2018-02-20 11:43:06 +0200

    added client & girl profile and fix

---
+++ src/components/profile/info_block.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import style from './info_block.css';
+import store from 'store/'
+
+class InfoBlock extends Component {
+	  printOptions(value, i) {
+      return (<div key={i} className={style.value}>{value}</div>)
+    }
+
+   render() {
+   	return (
+   		<div className={style.wrapper}>
+   			<div className={style.title}><span>{this.props.title}</span></div>
+          {
+              this.props.value.map((value, i) => this.printOptions(value, i))
+          }
+   		</div>
+   	);
+   }
+}
+
+const mapStateToProps = (state) => {
+    return state
+}
+
+export default connect(
+    mapStateToProps
+)(InfoBlock);
