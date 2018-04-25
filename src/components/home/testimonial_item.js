eaf5a35ea881f978655e30909930dc2530ba7132
jb.divart <jb.divart@gmail.com>
2018-04-25 14:27:14 +0300

    commit

--- src/components/home/testimonial_item.js
+++ src/components/home/testimonial_item.js
@@ -5,7 +5,7 @@ class TestimonialItem extends Component {
     render() {
         return (
                 <div className={style.testimonialBlock + ' text-center'} onClick={this.props.onClick}>
-                    <img src="http://mint.themes.tvda.pw/wp-content/uploads/2016/11/profile04-110x110.jpg" className={style.testimonialImg} alt="" />
+                    <img src="assets/img/testimonials-small.jpg" className={style.testimonialImg} alt="" />
                     <div className={style.testimonialStar}>
                         { Array.apply(null, Array(this.props.rating)).map((item, i) => <i key={i} className="fas fa-star"></i>) }
                     </div>
